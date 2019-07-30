0da221afee2ea18ca412cd7cd43a6444d4581cd7
Isabella Wang <wangxbella0108@gmail.com>
2019-07-30 01:16:34 -0400

    Updates

---
+++ precache-manifest.5b56216895c5cf9dea92ccb3b6ca7785.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29b9f435cbe6770ed9bc3084ccd199e",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "b67edb1bca89ca5d460a",
+    "url": "/ClickyGame/static/css/main.d06b99b6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ClickyGame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b67edb1bca89ca5d460a",
+    "url": "/ClickyGame/static/js/main.d402cd01.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
